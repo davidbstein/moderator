e7ec51d06ee2c4f5dea140078987edb354f60dc8
David Benjamin Stein <david.stein@appboy.com>
2017-01-25 17:41:44 +0000

    backwards compatibility via webpack. commented out viewports idea for the future

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git merge master --no-commit
 echo "!! compiling"
 export NODE_ENV="production"
 scss src/client/styles/moderator.scss:src/static/bundle.css
-./node_modules/.bin/webpack src/client/app.js src/static/bundle.js --optimize-minimize
+./node_modules/.bin/webpack src/client/app.js src/static/bundle.js --optimize-minimize --define process.env.NODE_ENV="'production'"
 export NODE_ENV="develop"
 git add src/static
 git commit -m '(automated) merge compile commit'
