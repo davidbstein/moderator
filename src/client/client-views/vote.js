ec77feead1ee284bc8727b8b002327e9fb7dd1b9
David Stein <davidbstein@gmail.com>
2017-03-15 13:29:24 -0400

    Update vote.js

--- src/client/client-views/vote.js
+++ src/client/client-views/vote.js
@@ -5,22 +5,16 @@ import {connect} from 'react-redux'
 
 export default class Vote extends React.Component {
   render() {
-    if (this.props.downvote) {
-      return <div className="vote">
-        <div className="upvote" onClick={this.props.upvote}>
-          <div className="upvote-arrow" />
-        </div>
-        <div className="vote-score"> {this.props.score} </div>
-        <div className="downvote" onClick={this.props.downvote}>
-          <div className="downvote-arrow" />
-        </div>
-      </div>
-    }
     return <div className="vote">
       <div className="upvote" onClick={this.props.upvote}>
         <div className="upvote-arrow" />
       </div>
       <div className="vote-score"> {this.props.score} </div>
+      {this.props.downvote && 
+        <div className="downvote" onClick={this.props.downvote}>
+          <div className="downvote-arrow" />
+        </div>
+      }
     </div>
   }
 }
